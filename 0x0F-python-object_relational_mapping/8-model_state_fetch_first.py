9a5328ff496f6893f71f716f6849fb4f450488cc
Lynnagneta <101558545+Lynnagneta@users.noreply.github.com>
2022-10-20 17:23:35 +0300

    Update

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
-""" print state object """
-
-from model_state import Base, State
+"""
+Prints the first State object from the database hbtn_0e_6_usa.
+Usage: ./8-model_state_fetch_first.py <mysql username> /
+                                      <mysql password> /
+                                      <database name>
+"""
+import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-import sys
+from model_state import State
 
-if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-    Sessionmaker = sessionmaker(bind=engine)
-    session = Sessionmaker()
-    response = session.query(State).first()
-    if response:
-        print("1: " + response.name)
-    else:
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).order_by(State.id).first()
+    if state is None:
         print("Nothing")
-    session.close()
+    else:
+        print("{}: {}".format(state.id, state.name))
